1663e9ca3b5bc6b48a0f9c72c1f527e85ee8c61b
Rohit B <rohitbanerje@gmail.com>
2022-02-06 21:35:46 -0500

    Spotify can be toggled when youtube is not playing

--- src/main.py
+++ src/main.py
@@ -25,19 +25,38 @@ async def get_media_info():
 
 
 async def toggle_spotify():
-    sessions = await MediaManager.request_async() #grab session manager instance
+    previous_state_spotify = 5
+    previous_state_chrome = 5
+    while True:
+        start = time.time()
+        sessions = await MediaManager.request_async() #grab session manager instance
 
-    all_sessions = sessions.get_sessions() #grab sequence of current instances
-    for current_session in all_sessions: #iterate and grab desired instances
-        if "chrome" in current_session.source_app_user_model_id.lower():
-            chrome_info = current_session.get_playback_info()
-        if "spotify" in current_session.source_app_user_model_id.lower():
-            spotify_manager = current_session 
-            spotify_info = current_session.get_playback_info()
-    if chrome_info.playback_status == 4 and spotify_info.playback_status == 4: #status of 4 is playing, 5 is paused
-        await spotify_manager.try_toggle_play_pause_async()
-    elif chrome_info.playback_status == 5 and spotify_info.playback_status == 5:
-        await spotify_manager.try_toggle_play_pause_async()
+        all_sessions = sessions.get_sessions() #grab sequence of current instances
+        for current_session in all_sessions: #iterate and grab desired instances
+            if "chrome" in current_session.source_app_user_model_id.lower():
+                chrome_info = current_session.get_playback_info()
+            if "spotify" in current_session.source_app_user_model_id.lower():
+                spotify_manager = current_session 
+                spotify_info = current_session.get_playback_info()
+        current_state_spotify = spotify_info.playback_status
+        current_state_chrome = chrome_info.playback_status
+        if current_state_chrome == 5:
+            if previous_state_chrome == 4 and previous_state_spotify == 4:
+                await spotify_manager.try_toggle_play_pause_async()
+            previous_state_chrome = chrome_info.playback_status
+            continue
+        elif current_state_chrome == 4: #status of 4 is playing, 5 is paused
+            if current_state_spotify == 4:
+                await spotify_manager.try_toggle_play_pause_async()
+                previous_state_spotify = 4
+            elif current_state_spotify == 5 and previous_state_chrome == 5:
+                previous_state_spotify = 5
+            previous_state_chrome = 4
+        previous_state_chrome = chrome_info.playback_status
+        end = time.time()
+        # previous_state_spotify = spotify_info.playback_status
+        time.sleep(1)
+        print(end-start)
         # print(f"chrome playback status: {chrome_info.playback_status}")
         # print(f"chrome playback status: {spotify_info.playback_status}")
         # print("+++++++++++++++++")
@@ -55,6 +74,4 @@ async def test_event():
 
 if __name__ == '__main__':
     #asyncio.run(get_media_info())
-    while True:
-        asyncio.run(toggle_spotify())
-        time.sleep(0.1)
+    asyncio.run(toggle_spotify())

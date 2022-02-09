46b5cefa167a8e8044060cb766cb6860e6c83eb1
Rohit B <rohitbanerje@gmail.com>
2022-02-09 00:59:49 -0500

    added comments and cleaned up depreciated funcs

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ from winrt.windows.media.control import \
 from winrt.windows.media.control import \
     PlaybackInfoChangedEventArgs as PlaybackEventArgs
 
-
+#this is a snippet of what other info is available from session instances
+#credit to tameTNT (https://stackoverflow.com/questions/65011660/how-can-i-get-the-title-of-the-currently-playing-media-in-windows-10-with-python)
 async def get_media_info():
     sessions = await MediaManager.request_async()
 
@@ -28,30 +29,26 @@ async def toggle_spotify():
     previous_state_spotify = 5
     previous_state_chrome = 5
     while True:
-        #start = time.time()
         sessions = await MediaManager.request_async() #grab session manager instance
 
         all_sessions = sessions.get_sessions() #grab sequence of current instances
         for current_session in all_sessions: #iterate and grab desired instances
             if "chrome" in current_session.source_app_user_model_id.lower():
-                #chrome_info = current_session.get_playback_info()
                 current_state_chrome = current_session.get_playback_info().playback_status
             if "spotify" in current_session.source_app_user_model_id.lower():
                 spotify_manager = current_session 
-                #spotify_info = current_session.get_playback_info()
                 current_state_spotify = current_session.get_playback_info().playback_status
-        # current_state_spotify = spotify_info.playback_status
-        # current_state_chrome = chrome_info.playback_status
-        if current_state_chrome == previous_state_chrome:#status of 4 is playing, 5 is paused
+        # state of 4 is playing, 5 is paused
+        if current_state_chrome == previous_state_chrome: #chrome state hasn't changed
             time.sleep(1)
             continue
-        elif current_state_chrome == 5 and previous_state_chrome == 4: #just paused
+        elif current_state_chrome == 5 and previous_state_chrome == 4: #chrome just paused
             if previous_state_spotify == 4:
                 await spotify_manager.try_toggle_play_pause_async()
             previous_state_chrome = current_state_chrome
             time.sleep(1)
             continue
-        elif current_state_chrome == 4 and previous_state_chrome == 5: #just started playing
+        elif current_state_chrome == 4 and previous_state_chrome == 5: #chrome just started playing
             if current_state_spotify == 4:
                 await spotify_manager.try_toggle_play_pause_async()
                 previous_state_spotify = 4
@@ -60,24 +57,9 @@ async def toggle_spotify():
             previous_state_chrome = 4
             time.sleep(1)
             continue
-        #end = time.time()
-        # previous_state_spotify = spotify_info.playback_status
-        #print(end-start)
         # print(f"chrome playback status: {chrome_info.playback_status}")
         # print(f"chrome playback status: {spotify_info.playback_status}")
         # print("+++++++++++++++++")
-        # time.sleep(2)
-
-
-async def test_event():
-    sessions = await MediaManager.request_async()
-    all_sessions = sessions.get_sessions()
-    for current_session in all_sessions:  # there needs to be a media session running
-        if "chrome" in current_session.source_app_user_model_id.lower():
-            chrome_mananger = current_session
-        if "spotify" in current_session.source_app_user_model_id.lower():
-            spotify_info = current_session
 
 if __name__ == '__main__':
-    #asyncio.run(get_media_info())
     asyncio.run(toggle_spotify())

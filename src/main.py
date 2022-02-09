2d26414a1c8a6c37ff17a75347c3760a6f992099
Rohit B <rohitbanerje@gmail.com>
2022-02-09 00:55:00 -0500

    reworked logic to three simple cases

--- src/main.py
+++ src/main.py
@@ -34,28 +34,34 @@ async def toggle_spotify():
         all_sessions = sessions.get_sessions() #grab sequence of current instances
         for current_session in all_sessions: #iterate and grab desired instances
             if "chrome" in current_session.source_app_user_model_id.lower():
-                chrome_info = current_session.get_playback_info()
+                #chrome_info = current_session.get_playback_info()
+                current_state_chrome = current_session.get_playback_info().playback_status
             if "spotify" in current_session.source_app_user_model_id.lower():
                 spotify_manager = current_session 
-                spotify_info = current_session.get_playback_info()
-        current_state_spotify = spotify_info.playback_status
-        current_state_chrome = chrome_info.playback_status
-        if current_state_chrome == 5:
-            if previous_state_chrome == 4 and previous_state_spotify == 4:
-                await spotify_manager.try_toggle_play_pause_async()
-            previous_state_chrome = chrome_info.playback_status
+                #spotify_info = current_session.get_playback_info()
+                current_state_spotify = current_session.get_playback_info().playback_status
+        # current_state_spotify = spotify_info.playback_status
+        # current_state_chrome = chrome_info.playback_status
+        if current_state_chrome == previous_state_chrome:#status of 4 is playing, 5 is paused
+            time.sleep(1)
             continue
-        elif current_state_chrome == 4: #status of 4 is playing, 5 is paused
+        elif current_state_chrome == 5 and previous_state_chrome == 4: #just paused
+            if previous_state_spotify == 4:
+                await spotify_manager.try_toggle_play_pause_async()
+            previous_state_chrome = current_state_chrome
+            time.sleep(1)
+            continue
+        elif current_state_chrome == 4 and previous_state_chrome == 5: #just started playing
             if current_state_spotify == 4:
                 await spotify_manager.try_toggle_play_pause_async()
                 previous_state_spotify = 4
-            elif current_state_spotify == 5 and previous_state_chrome == 5:
+            else:
                 previous_state_spotify = 5
             previous_state_chrome = 4
-        previous_state_chrome = chrome_info.playback_status
+            time.sleep(1)
+            continue
         #end = time.time()
         # previous_state_spotify = spotify_info.playback_status
-        time.sleep(1)
         #print(end-start)
         # print(f"chrome playback status: {chrome_info.playback_status}")
         # print(f"chrome playback status: {spotify_info.playback_status}")
